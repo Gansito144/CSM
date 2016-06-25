5c5c3f91314627356e919c2307298c8d074d7e90
Ulises Mendez Martinez <ulisesmdzmtz@gmail.com>
2016-06-24 20:26:57 -0500

    Add method selector based on first byte
    * TODO Double check if the right byte is 0 or 3
    
    Signed-off-by: Ulises Mendez Martinez <ulisesmdzmtz@gmail.com>

--- subjects/ADMPC201601/avr_hex_decoder.cpp
+++ subjects/ADMPC201601/avr_hex_decoder.cpp
@@ -9,11 +9,12 @@
 #define getHex(s,n) (sscanf(s,"%x",&(n)))
 
 // Macros for bits manipulation
-#define getByte(w,B) (((w)&((0xFF)<<B))>>B)
+#define getByte(w,B) (((w)&((0xF)<<B))>>B)
 #define getBit(w,b) (((w)&((0x1)<<b))>>b)
 
 using namespace std;
 
+// Just for debugging process
 #if 1
 char logs[123456];
 #define DEBUG(...) sprintf(logs,__VA_ARGS__)
@@ -21,24 +22,24 @@ char logs[123456];
 #define DEBUG printf
 #endif
 
+// Return true when the checksum of hex line is OK
 int checksum_ok(string &str, int &size, int &checkSum) {
 	unsigned int check=0;
 	int twoByte, i=1;
 	for(; i< size; i += 2) {
-		DEBUG("i(%d): ",i);
 		getHex(to_c(cut(str,i,2)),twoByte);
 		check += twoByte;
 		check &= 0xFF;
-		DEBUG("%x %d %x\n",twoByte,twoByte,check);
+		DEBUG("i(%d) %x %d %x\n",i,twoByte,twoByte,check);
 	}
 	// Two complement of a function
 	check = (~check) + 1;
 	check &= 0xFF;
-
 	// Verify against cheksum expected
 	return (check != checkSum) ? -EINVAL : 0;
 }
 
+// Separates commands from each hex line 
 int parse_cmd(string &str, string &cmds) {
 	int byteCount, address, recordType;
 	int checkSum, idx = 1;
@@ -96,9 +97,38 @@ int parse_cmd(string &str, string &cmds) {
 	return checksum_ok(str, idx, checkSum);
 }
 
+void opAxzz0(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzz1(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzz2(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzz3(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzz4(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzz5(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzz6(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzz7(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzz8(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzz9(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzzA(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzzB(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzzC(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzzD(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzzE(int &opCode){printf("%s\n",__FUNCTION__);}
+void opAxzzF(int &opCode){printf("%s\n",__FUNCTION__);}
+
+// Axzz Array to select the right instruction
+typedef void (*Handler)(int &opCode);
+Handler Axzz[] = {
+    opAxzz0, opAxzz1, opAxzz2, opAxzz3,
+    opAxzz4, opAxzz5, opAxzz6, opAxzz7,
+    opAxzz8, opAxzz9, opAxzzA, opAxzzB,
+    opAxzzC, opAxzzD, opAxzzE, opAxzzF
+};
+
+// Choose the right command to execute based on some reduction  
 void execute_cmd( int &opCode ) {
-	printf("%02X %02X %02X %02X\n",getByte(opCode,0),getByte(opCode,1),getByte(opCode,2),getByte(opCode,3));
-	printf("%02X %02X %02X %02X\n",getByte(opCode,3),getByte(opCode,2),getByte(opCode,1),getByte(opCode,0));
+	int A = getByte(opCode,3);
+	// Delegate the responsability to next functions
+	printf("A (%02d) (%x)",A,A);
+	Axzz[A](opCode);
 }
 
 
