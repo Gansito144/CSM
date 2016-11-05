f0c06e55d87c6d4628b791fc5b01b2a71f899c9e
Ulises Mendez Martinez <ulisesmdzmtz@gmail.com>
2016-11-05 14:56:46 -0600

    Fisrt commit about perceptron
    Signed-off-by: Ulises Mendez Martinez <ulisesmdzmtz@gmail.com>

---
+++ subjects/Pattern-Recognition/homeworks/perceptron.py
@@ -0,0 +1,9 @@
+def perceptron():
+  tries = 1000  # Do not do more iterations than tries
+  while tries > 0 :
+    print tries
+    tries = tries - 1
+  print "Hello World!"
+
+if __name__ == '__main__':
+  perceptron()
